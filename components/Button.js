5d9e15aa0c58c5a4d52b1b3c166e1dd7e5eecd64
Hafizul <fizultm@gmail.com>
2021-06-01 20:59:59 +0800

    Add trends section

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,7 @@
 function Button({ text }) {
   return (
     <div>
-      <p className="uppercase cursor-pointer font-bold px-8 py-2 text-xs w-max border border-black rounded-full hover:bg-gray-300">
+      <p className="uppercase cursor-pointer font-bold px-8 py-2 text-xs w-full border border-black rounded-full hover:bg-gray-300 text-center">
         {text}
       </p>
     </div>
