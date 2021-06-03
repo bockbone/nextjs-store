0869e0e2b9af5913aa6db0ba85cd46a22fafac38
Hafizul <fizultm@gmail.com>
2021-06-04 02:38:06 +0800

    Add latest purchase list section

--- components/dashboard/DashboardContent.js
+++ components/dashboard/DashboardContent.js
@@ -11,7 +11,7 @@ function DashboardContent() {
   console.log(isOpen);
 
   return (
-    <div className="flex-auto font-mainFont">
+    <div className="flex-auto font-mainFont max-h-screen overflow-auto">
       <div className="flex flex-col">
         <div className="flex justify-between my-4 mx-16 items-center">
           <h2 className="text-3xl font-bold text-[#362F78]">Dashboard</h2>
@@ -79,7 +79,166 @@ function DashboardContent() {
             </div>
           </div>
         </div>
-        <div>bottom</div>
+        <div className="mx-16 my-10 ">
+          <p className="text-base font-bold mb-4 text-gray-400">
+            Latest Purchase
+          </p>
+          <div className="">
+            <div className="flex font-bold my-4 p-2">
+              <p className="w-1/5">
+                Name{" "}
+                <span className="italic text-sm text-gray-400">Total - 78</span>
+              </p>
+              <p className="w-1/5">Product</p>
+              <p className="w-1/5">Purchase Date</p>
+              <p className="w-1/5">Status</p>
+              <p className="w-1/5">Shipping Status</p>
+            </div>
+            <div className="flex text-gray-500 hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition h-12 items-center">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <div className="w-1/5">
+                <p className="text-white text-xs bg-red-600 w-min py-1 px-3 rounded-full">
+                  Unpaid
+                </p>
+              </div>
+              <div className="w-1/5">
+                <p className="text-white text-xs bg-yellow-600 w-max py-1 px-3 rounded-full">
+                  In-progress
+                </p>
+              </div>
+            </div>
+            <div className="flex text-gray-700 hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition h-12 items-center">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <div className="w-1/5">
+                <p className="text-white text-xs bg-green-500 w-min py-1 px-3 rounded-full">
+                  Paid
+                </p>
+              </div>
+              <div className="w-1/5">
+                <p className="text-white text-xs bg-green-500 w-max py-1 px-3 rounded-full">
+                  Shipped
+                </p>
+              </div>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+            <div className="flex hover:bg-gray-200 cursor-pointer py-3 px-2 rounded-md transition">
+              <p className="w-1/5">Hafizul Hassan</p>
+              <p className="w-1/5">Hoodie,cap</p>
+              <p className="w-1/5">11 Oct 2021</p>
+              <p className="w-1/5">Unpaid</p>
+              <p className="w-1/5">Unavailable</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
