9b66c6f11b626ba35a852cf85f84faa39b857262
Hafizul <fizultm@gmail.com>
2021-06-03 16:56:17 +0800

    Update carousel

--- components/HomepageComponent/Carousel.js
+++ components/HomepageComponent/Carousel.js
@@ -7,13 +7,17 @@ function MyCarousel() {
   return (
     <div className="max-w-screen-mediumlg mx-auto">
       <div className="mx-5 xl:mx-0 my-20">
-        <Swiper spaceBetween={30} pagination={{ clickable: true }}>
+        <Swiper
+          spaceBetween={30}
+          pagination={{ clickable: true }}
+          className="rounded-xl"
+        >
           <SwiperSlide>
-            <img className="rounded-xl" src="/slide-one.jpg" />
+            <img src="/slide-one.jpg" />
           </SwiperSlide>
 
           <SwiperSlide>
-            <img className="rounded-xl" src="/slide-two.jpg" />
+            <img src="/slide-two.jpg" />
           </SwiperSlide>
         </Swiper>
       </div>
