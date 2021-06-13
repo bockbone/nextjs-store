675a7fe642d99018e93632fc1c17251e7f981ffb
Hafizul Hassan <36685009+bockbone@users.noreply.github.com>
2021-06-13 20:35:06 +0800

    Update config

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    MONGO_URI:
-      "mongodb+srv://dbNextjsStore:<password>@nextjs-store.b3oab.mongodb.net/nextjsStore?retryWrites=true&w=majority",
-    MONGO_PASS: "1234makan",
-  },
-};
