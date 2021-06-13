42bc73a9f4ee3d5e155ddf73aa2a2b5b81a1866c
Hafizul Hassan <36685009+bockbone@users.noreply.github.com>
2021-06-13 20:33:30 +0800

    Add product API for GET all products, POST a product

--- models/Product.js
+++ models/Product.js
@@ -6,6 +6,29 @@ const ProductSchema = new mongoose.Schema({
     required: [true, "Please provide a name for the product"],
     maxlength: [80, "Name cannot be more than 80 characters"],
   },
+  sku: {
+    type: Number,
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description for the product"],
+  },
+  brand: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: [true, "Please provide a category for the product"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please provide a stock quantity for the product"],
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  images: [String],
 });
 
 export default mongoose.models.Product ||
